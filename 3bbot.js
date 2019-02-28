6ed4e6eb811c61b64987ca9601892f6c380eef80
lucas-walter <wal18388@spengergasse.at>
2019-02-28 23:06:57 +0100

    Add safe warning

--- 3bbot.js
+++ 3bbot.js
@@ -12,6 +12,8 @@ client.on('ready', () => {
 client.on('error', e => {});
 
 client.on('message', msg => {
+	if (msg.startsWith("!")) msg.reply("3BBot läuft im **Safe Mode**.").delete(5000);
+	
   if (!msg.author.bot && msg.content.startsWith('!hü')) {
 	  if (msg.content.match(/!hü (<#\d*>) (.*) bis (\S).*/gi)) {
 		  var hueregex = /!hü <#(\d*)> (.*):(.*) bis (.*).*/gi
