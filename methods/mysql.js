2486f73d55eec7b04b49021f7057380cbd0609b4
lucas-walter <wal18388@spengergasse.at>
2019-07-30 00:54:00 +0200

    Add more MySQL Functions

--- methods/mysql.js
+++ methods/mysql.js
@@ -1,6 +1,15 @@
+/*
+ * mysql.js - Copyright (C) 2019 Lucas Walter - All Rights Reserved
+ * MySQL Handler for 4BBot
+ */
+
 var cfg = require("../config");
 var mysql = require("mysql");
 
+/**
+ * Inserts a Homework Object into the Database
+ * @param {Homework} homework The Homework Object to insert into the Database
+ */
 function insertHomework(homework) {
     let connection = mysql.createConnection(cfg.mysql);
     let sql;
@@ -11,6 +20,21 @@ function insertHomework(homework) {
     connection.end();
 }
 
+/**
+ * Inserts a Termin Object into the Database
+ * @param {Termin} termin The Termin Object to insert into the Database
+ */
+function insertTermin(termin) {
+    let connection = mysql.createConnection(cfg.mysql);
+    let sql;termin
+    if (homework.gooddate) sql = `INSERT INTO termine(fach, titel, beschreibung, am, vom, amdatum) VALUES('${termin.subject}', '${termin.title}', '${termin.description}', '${termin.until}', NOW(), '${termin.until}')`
+    else sql = `INSERT INTO aufgaben(fach, titel, beschreibung, am, vom) VALUES('${termin.subject}', '${termin.title}', '${termin.description}', '${termin.until}', NOW())`
+    console.log(sql);
+    connection.query(sql);
+    connection.end();
+}
+
 module.exports = {
-    insertHomework : insertHomework
+    insertHomework : insertHomework,
+    insertTermin : insertTermin
 }
